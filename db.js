14c0abba76f4230e6a08527a01abf8f0402a3267
loksblnine <illya01@ukr.net>
2021-07-22 23:45:15 +0300

    try to fix SSL error

--- db.js
+++ db.js
@@ -8,7 +8,9 @@ const pool = new Pool(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         database: process.env.DB_DATABASE,
-        ssl: process.env.DB_SSL === 'true'
+        ssl: {
+            rejectUnauthorized: false,
+        }
     }
 )
 
