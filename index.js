38e03058452b77230c8c1328f05aaf1dd41337f2
loksblnine <illya01@ukr.net>
2021-08-16 12:38:08 +0300

    try feature: sendgrid

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db")
 require("dotenv").config();
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SG_API_KEY)
 
 //middleware
 app.use(cors())
@@ -225,18 +227,19 @@ app.delete('/orders/:id', async (request, response) => {
 
 //region send mail
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: process.env.USER,
-        pass: process.env.PASSWORD,
-        accessToken: process.env.OAUTH_ACCESS_TOKEN,
-        clientId: process.env.OAUTH_CLIENT_ID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    },
-});
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         type: "OAuth2",
+//         user: process.env.USER,
+//         pass: process.env.PASSWORD,
+//         accessToken: process.env.OAUTH_ACCESS_TOKEN,
+//         clientId: process.env.OAUTH_CLIENT_ID,
+//         clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+//     },
+// });
 
 app.get('/send', async (request, response) => {
     try {
@@ -255,19 +258,28 @@ app.post("/send", function (req, res) {
         subject: 'Clockwise test mail 123',
         text: req.body.message,
     };
+    sgMail
+        .send(email)
+        .then((response) => {
+            console.log(response[0].statusCode)
+            console.log(response[0].headers)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 
-    transporter.sendMail(email, function (err, data) {
-        if (err) {
-            res.json({
-                status: "fail",
-            });
-        } else {
-            console.log("== Message Sent ==");
-            res.json({
-                status: "success",
-            });
-        }
-    });
+    // transporter.sendMail(email, function (err, data) {
+    //     if (err) {
+    //         res.json({
+    //             status: "fail",
+    //         });
+    //     } else {
+    //         console.log("== Message Sent ==");
+    //         res.json({
+    //             status: "success",
+    //         });
+    //     }
+    // });
 });
 
 //endregion
