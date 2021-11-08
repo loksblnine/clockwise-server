e2661ec99f0965b66461aa0c5484aec16e62753d
loksblnine <illya01@ukr.net>
2021-11-08 12:58:00 +0200

    add tests for isAdmin() isRankingValid()

--- test/example.test.js
+++ test/example.test.js
@@ -2,10 +2,13 @@ const validation = require('../index')
 const expect = require('chai').expect;
 
 describe('Test validation', () => {
-    describe('Test validation.isNameValid()', ()=>
-    {
+    describe('Test validation.isNameValid()', () => {
         it('should return true', () => {
             const result = validation.isNameValid("Илюхан")
+            expect(result).to.be.true;
+        });
+        it('should return true', () => {
+            const result = validation.isNameValid("Илюха Bkmz")
             expect(result).to.be.true;
         });
         it('should return false', () => {
@@ -13,8 +16,7 @@ describe('Test validation', () => {
             expect(result).to.be.false;
         });
     })
-    describe('Test validation.isEmailValid()', ()=>
-    {
+    describe('Test validation.isEmailValid()', () => {
         it('should return true', () => {
             const result = validation.isEmailValid("admin@example.com")
             expect(result).to.be.true;
@@ -24,8 +26,7 @@ describe('Test validation', () => {
             expect(result).to.be.false;
         });
     })
-    describe('Test validation.isDateValid()', ()=>
-    {
+    describe('Test validation.isDateValid()', () => {
         it('should return false', () => {
             const result = validation.isDateValid("2021-10-10T14:00:00.000000")
             expect(result).to.be.false;
@@ -37,8 +38,45 @@ describe('Test validation', () => {
         it('should return false', () => {
             const result = validation.isDateValid("2021-11-26T4:00:00.000000")
             expect(result).to.be.false;
-        }); it('should return false', () => {
+        });
+        it('should return false', () => {
             const result = validation.isDateValid("2021-10-26T11:00:00.000Z")
+            expect(result).to.be.false;
+        });
+    })
+    describe('Test validation.isRankingValid()', () => {
+        it('should return false', () => {
+            const result = validation.isRankingValid("0")
+            expect(result).to.be.false;
+        });
+        it('should return false', () => {
+            const result = validation.isRankingValid("8")
+            expect(result).to.be.false;
+        });
+        it('should return false', () => {
+            const result = validation.isRankingValid("-4")
+            expect(result).to.be.false;
+        });
+        it('should return false', () => {
+            const result = validation.isRankingValid("3,5")
+            expect(result).to.be.false;
+        });
+        it('should return true', () => {
+            const result = validation.isRankingValid("3.5")
+            expect(result).to.be.true;
+        });
+        it('should return true', () => {
+            const result = validation.isRankingValid("4.85")
+            expect(result).to.be.true;
+        });
+    })
+    describe('Test validation.isAdmin()', () => {
+        it('should return false', () => {
+            const result = validation.isAdmin("randomwrong")
+            expect(result).to.be.false;
+        });
+        it('should return false', () => {
+            const result = validation.isAdmin("сука неправильный имеил)")
             expect(result).to.be.false;
         });
     })
