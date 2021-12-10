1ef9f69151bfcc640213e2cb8041dcb923fafe02
loksblnine <illia.zinchenko@clockwise.software>
2021-12-10 13:06:48 +0200

    try to fix deploy

--- database/config/config.js
+++ database/config/config.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 module.exports = db
