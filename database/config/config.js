306139b3ced78d721dfc40a6e842ee70c1e2474a
loksblnine <illia.zinchenko@clockwise.software>
2021-12-10 13:23:16 +0200

    try to fix deploy

--- database/config/config.js
+++ database/config/config.js
@@ -4,8 +4,11 @@ require('dotenv').config()
 const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
-        ssl: true
-    }
+        ssl: {
+            require: true, // This will help you. But you will see nwe error
+            rejectUnauthorized: false // This line will fix new error
+        }
+    },
 });
 
 module.exports = db
