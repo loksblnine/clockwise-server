94caeaabc3338c1e0ee0043a805d557d9bd02a4b
loksblnine <illya01@ukr.net>
2021-12-29 10:13:26 +0200

    try to migrations

---
+++ src/database/migrations/20211229075857-create-blog.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Blogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      article_id: {
+        type: Sequelize.INTEGER
+      },
+      header: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Blogs');
+  }
+};
